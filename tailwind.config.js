b8bdf32e186d7cac674d95a260f57105e05c187d
Zohaib haider <zebihaider123@gmail.com>
2023-10-24 17:16:11 +0500

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: [
+    "./portfolio.html",
+    "./portfolio.js",
+    "./input.css",
+    "./output.css",
+  ],
   theme: {
     extend: {
       fontFamily: {
+        'sans': ['Inter', 'Helvetica', 'Arial', 'sans'],
+        'serif': ['Great Vibes', 'serif'],
         custom: ['Poppins', 'sans'],
         custom1: ['Russo one', 'sans'],
       },
+      screens: {
+        'sssm': '300px',
+        'ssm': '500px',
+        'smd': '700px',
+        'md': '900px',
+        'xl': '1240px',
+      },
+      borderWidth: {
+        '1': '1px',
+      },
+
       boxShadow: {
-        'sm': '0 1px 4px 0 rgba(0, 0, 0, 0.08)',
+        'sm': '0 2px 4px 0 rgba(0, 0, 0, 0.1)',
         'lg': '0 3px 4px 0 rgba(0, 0, 0, 0.08)'
       },
+    },
+    container: {
+      center: true,
     },
   },
   plugins: [],
 }
+
 
